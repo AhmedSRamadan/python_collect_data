63dbcb9afb569f06cfd0e54838b6c3b9bc6a49a6
Ahmed Ramadan <ahmed.sramadan89@gmail.com>
2020-05-09 09:45:48 +0200

    user data script and error in docker-compose build

---
+++ script.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+export DOCKER_HOST=127.0.0.1
+
+sudo yum update -y
+
+sudo yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+
+sudo yum install -y yum-utils
+
+sudo yum-config-manager \
+    --add-repo \
+    https://download.docker.com/linux/centos/docker-ce.repo
+
+sudo yum-config-manager --enable docker-ce-nightly
+
+sudo yum install -y docker-ce docker-ce-cli containerd.io
+
+sudo systemctl start docker
+
+sudo systemctl enable docker
+
+sudo usermod -aG docker $(whoami)
+
+
+sudo curl -L "https://github.com/docker/compose/releases/download/1.25.5/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+
+sudo chmod +x /usr/local/bin/docker-compose
+
+sudo yum install git -y
+
+git clone https://github.com/AhmedSRamadan/python_collect_data.git
+
+cd ./python_collect_data
+
+docker-compose build .
+
+sudo service docker start && docker-compose up -d
